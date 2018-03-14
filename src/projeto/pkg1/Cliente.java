1f193c7256f7ef054dc1244162ec464ab8c45307
gdev <guilhermefos.developer@gmail.com>
2018-03-13 21:45:06 -0300

    Primeiro commit

---
+++ src/projeto/pkg1/Cliente.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto.pkg1;
+
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author gdev
+ */
+public class Cliente {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
